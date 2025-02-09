9402b62e92ddeb8814d776cdb3883e941f0f66ac
Tony <t4tonykuriakose@gmail.com>
2025-02-09 10:40:13 +0530

    result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useContext, useState } from 'react'
+import { assets } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Result = () => {
 
+  const [image,setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded,setIsImageLoaded] = useState(false);
+  const [loading,setLoading] =  useState(false);
+  const [input,setInput] = useState('');
+  const submitHanlder = async(e)=>{
+
+    
+
+  }
+
+
 
   return (
-    <div>
-      <p>Result Component</p>
-    </div>
-  );
-};
+    <motion.form className='flex flex-col min-h-[90vh] justify-center items-center'
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+
+      <div>
+        <div className='relative'>
+          <img src={assets.sample_img_1} className='max-w-sm rounded'  alt="" />
+          
+        </div>
+        <p className={!loading ? 'hidden':''}>Loading.....</p>
+  
+      </div>
+      {!isImageLoaded && <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
+        <input onChange={e => setInput(e.target.value)} value={input} className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 grey-placeholder' type="text" placeholder='Describe what you want to generate' />
+        <button type='submit' className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
+      </div>}
+
+      {isImageLoaded && <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
+        <p onClick={() => { setIsImageLoaded(false) }} className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
+        <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
+      </div>}
+      
+
+    </motion.form>
+  )
+}
 
 export default Result;
