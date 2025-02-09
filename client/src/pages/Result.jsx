c9620f54748de9cae0b86877522c98b0eeaab95c
Tony <t4tonykuriakose@gmail.com>
2025-02-09 18:04:40 +0530

    feature:front end completed

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,23 +1,33 @@
 import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 import { motion } from 'framer-motion'
 
 const Result = () => {
 
-  const [image,setImage] = useState(assets.sample_img_1);
-  const [isImageLoaded,setIsImageLoaded] = useState(false);
-  const [loading,setLoading] =  useState(false);
-  const [input,setInput] = useState('');
-  const submitHanlder = async(e)=>{
+  const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
+  const [image, setImage] = useState(assets.sample_img_1)
 
-    
+  const { generateImage } = useContext(AppContext)
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    if (input) {
+      const image = await generateImage(input)
+      if (image) {
+        setIsImageLoaded(true)
+        setImage(image)
+      }
+    }
+    setLoading(false)
   }
 
-
-
   return (
-    <motion.form className='flex flex-col min-h-[90vh] justify-center items-center'
+    <motion.form onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'
       initial={{ opacity: 0.2, y: 100 }}
       transition={{ duration: 1 }}
       whileInView={{ opacity: 1, y: 0 }}
@@ -26,12 +36,12 @@ const Result = () => {
 
       <div>
         <div className='relative'>
-          <img src={assets.sample_img_1} className='max-w-sm rounded'  alt="" />
-          
+          <img className='max-w-sm rounded' src={image} alt="" />
+          <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading ? 'w-full transition-all duration-[10s]' : 'w-0'}`} />
         </div>
-        <p className={!loading ? 'hidden':''}>Loading.....</p>
-  
+        <p className={!loading ? 'hidden' : ''}>Loading.....</p>
       </div>
+
       {!isImageLoaded && <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
         <input onChange={e => setInput(e.target.value)} value={input} className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 grey-placeholder' type="text" placeholder='Describe what you want to generate' />
         <button type='submit' className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
@@ -41,7 +51,6 @@ const Result = () => {
         <p onClick={() => { setIsImageLoaded(false) }} className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
         <a href={image} download className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
       </div>}
-      
 
     </motion.form>
   )
