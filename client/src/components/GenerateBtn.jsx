78d1bb9edf3261c347aa675f2a910e8c31f61f3c
Tony <t4tonykuriakose@gmail.com>
2025-02-09 08:29:13 +0530

    feature:generate button

---
+++ client/src/components/GenerateBtn.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { motion } from 'framer-motion'
+import { assets } from '../assets/assets'
+
+const GenerateBtn = () => {
+
+ 
+
+    return (
+        <motion.div
+            className='pb-16 text-center'
+            initial={{ opacity: 0.2, y: 100 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+        >
+        
+            <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6 md:py-16'>See the magic. Try now</h1>
+
+            <button className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500'>
+                Generate Images <img className='h-6' src={assets.star_group} alt="" />
+            </button>
+
+        </motion.div>
+    )
+}
+
+export default GenerateBtn
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header";
 import Steps from "../components/Steps";
 import Description from "../components/Description";
+import Testimonials from "../components/Testimonials";
+import GenerateBtn from "../components/GenerateBtn";
 
 const Home = () => {
 
@@ -11,6 +13,8 @@ const Home = () => {
       <Header />
       <Steps />
       <Description />
+      <Testimonials />
+      <GenerateBtn />
      
     </div>
   );
