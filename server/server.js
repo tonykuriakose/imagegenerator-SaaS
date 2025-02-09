481530d3fbaec1385b80e30923367468fb266b3e
Tony <t4tonykuriakose@gmail.com>
2025-02-09 18:34:46 +0530

    feature:DB Connected

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 import 'dotenv/config';
 import express from 'express'
 import cors from 'cors'
+import connectDB from './config/mongodb.js';
 
-
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 2999
 const app = express();
+await connectDB()
 
 app.use(express.json())
 app.use(cors())
 
 
-
 app.get('/', (req,res) => res.send("API Working"))
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
-
-
